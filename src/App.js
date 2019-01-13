0f622961695214c50ee1e0931fe99a69e0a30d56
Kenny Howard <ken.chad.howard@gmail.com>
2019-01-13 01:59:26 -0700

    updating handle state change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentItem: '',
+      username: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div className='app'>
@@ -14,8 +30,8 @@ class App extends Component {
         <div className='container'>
           <section className='add-item'>
               <form>
-                <input type="text" name="username" placeholder="Name..." />
-                <input type="text" name="currentItem" placeholder="Item..." />
+                <input type="text" name="username" placeholder="Name..." onChange={this.handleChange} value={this.state.username} />
+                <input type="text" name="currentItem" placeholder="Item..." onChange={this.handleChange} value={this.state.currentItem} />
                 <button>Add Item</button>
               </form>
           </section>
