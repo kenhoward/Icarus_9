fe46a8a7f6488d78e7169f201bb20fd374bbfcd5
Kenny Howard <ken.chad.howard@gmail.com>
2019-01-14 22:45:18 -0700

    typo

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
         <ul>
           <li key={item.id}>
             <h3>{item.title}</h3>
-            <p>item: {item.users}</p>
+            <p>item: {item.user}</p>
           </li>
         </ul>
       )
